db6ef9444f7228c1970b33063f6688bf691b1d15
yanniep <107048160+yanniep@users.noreply.github.com>
2026-04-18 20:17:20 +0800

    homepage testing

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,49 +1,28 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import { useState } from "react";
+import{
+  Navbar, Container, Nav, NavDropdown
+} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import icon from "app/assets/icon.jpg";
 
 export function Welcome({ message }: { message: string }) {
 	return (
-		<main className="flex items-center justify-center pt-16 pb-4">
-			<div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-				<header className="flex flex-col items-center gap-9">
-					<div className="w-[500px] max-w-[100vw] p-4">
-						<img
-							src={logoLight}
-							alt="React Router"
-							className="block w-full dark:hidden"
-						/>
-						<img
-							src={logoDark}
-							alt="React Router"
-							className="hidden w-full dark:block"
-						/>
-					</div>
-				</header>
-				<div className="max-w-[300px] w-full space-y-6 px-4">
-					<nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-						<p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-							What&apos;s next?
-						</p>
-						<ul>
-							{resources.map(({ href, text, icon }) => (
-								<li key={href}>
-									<a
-										className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-										href={href}
-										target="_blank"
-										rel="noreferrer"
-									>
-										{icon}
-										{text}
-									</a>
-								</li>
-							))}
-							<li className="self-stretch p-3 leading-normal">{message}</li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</main>
+		<>
+			{navbar()}
+      {
+        lang === "ZH" ? introductionZH() : introductionEN()
+      }
+      <section id="center">
+        <div className="star">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="-100 -100 200 200" width="200" height="200">
+          <polygon points="0,-100 29.39,-40.45 95.11,-30.9 47.55,15.45 58.78,80.90 0,50 -58.78,80.9 -47.55,15.45 -95.11,-30.9 -29.39,-40.45" />
+        </svg>
+        </div>
+        
+      </section>
+	  </>
 	);
 }
 
@@ -88,3 +67,131 @@ const resources = [
 		),
 	},
 ];
+
+let lang = "ZH";
+const updateLang = (changeLang: string) => {
+  lang = changeLang;
+};
+
+function navbar(){
+  const [language, setLangCode] = useState("");
+
+  const changeLanguage = () => {
+    setLangCode(language.trim() === "ZH" ? "EN" : "ZH");
+    updateLang(language);
+  };
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Text onClick={changeLanguage} style={{cursor: "pointer"}}  >
+          {
+            language === "ZH" ? "中文" : "English"
+          }
+        </Navbar.Text>
+      </Container>
+    </Navbar>
+  );
+}
+
+function introductionEN(){
+  return (
+    <div id="introduction" className="introduction">
+      <h1>Hello, I'm Pumpaa, a 100% I-person(?)
+      </h1>
+	  <img className="profile_icon" src='..\app\assets\icon.jpg' alt="profile" width="200" height="200"/>
+      <p>I'm from Hong Kong and a native speaker of Cantonese.<br/>
+      I can understand English, Mandarin and a little bit of Japanese. Currently studying Italian on Duolingo<br/>
+      Following are some of my favorite things that you may be interested in:
+      </p>
+        <ul className="center-list">
+          <li>Holostars
+            <ul className="holostars_list">
+              <li>Hanasaki Miyabi</li>
+              <li>Hizaki Gamma</li>
+              <li>Machina X Flayon</li>
+              <li>All Holostars' members</li>
+            </ul>
+          </li>
+          <li>NU:Carnival
+            <ul className="canival_list">
+              <li>Kuya</li>
+              <li>Rei</li>
+              <li>Lin</li>
+            </ul>
+          </li>
+          <li>Anime / Novel 
+            <ul>
+              <li>Natsume's Book of Friends</li>
+              <li>EIGHTY SIX</li>
+              <li>Omniscient Reader's Viewpoint</li>
+              <li>Debut or Die</li>
+            </ul>
+          </li>
+          <li>Cinnamoroll</li>
+        </ul>
+        <p>I'm open to any fandom, but I'm not good at chatting, a bit negative and you may find me a bit weird(?).<br/>
+        Despite that, I'm still looking forward to communicating with friends who share the same interests.<br/>
+        Nice to meet you!</p>
+		<h2>By the way, I hate it when people say my oshi is bad without logic or evidence^^</h2>
+    </div>
+  );
+}
+
+function introductionZH(){  
+  return (
+    <div id="introduction" className="introduction">
+      <h1>你好，我是純種I人(?)Pumpaa！</h1>
+      <img className="profile_icon" src='..\app\assets\icon.jpg' alt="profile" width="200" height="200"/>
+      <p>我是香港人，主使用講廣東話。<br/>
+      另外我能理解英語、國語和一點點日語。目前在使用情勒鳥學習意大利文<br/>
+      以下是一些我有坑的團體、角色和作品：
+      </p>
+        <ul className="center-list">
+          <li>Holostars
+            <ul className="holostars_list">
+              <li>花咲みやび</li>
+              <li>緋崎ガンマ</li>
+              <li>Machina X Flayon</li> {/* マキナ・X・フレオン */}
+              <li>所有Holostars成員</li>  
+            </ul>
+          </li>
+          <li>NU:Carnival
+            <ul className="canival_list">
+              <li>玖夜</li>
+              <li>歛</li>
+              <li>環</li>
+            </ul>
+          </li>
+          <li>動漫、小說
+            <ul>
+              <li>夏目友人帳</li>
+              <li>86 -不存在的戰區-</li>
+              <li>全知讀者視角</li>
+              <li>得了不出道就會死的病</li>
+            </ul>
+          </li>
+          <li>肉桂狗/大耳狗</li>
+        </ul>
+        <p>我不拒同擔，但不太會聊天，人有點負面，然後你可能會覺得我人有點奇怪(?)<br/>
+        雖然如此，還是很期待能和同坑的朋友交流。<br/>
+        還請多多指教！</p>
+		<h2>對了，我最討厭別人沒有邏輯或證據地說我的推不好^^</h2>
+    </div>
+  );
+}   
+
